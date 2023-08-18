d2ea9eae40189599185960433f02c29796bbde4c
guilhermesales15 <guilhermefeit4@gmail.com>
2023-08-18 20:22:56 -0300

    Criando projeto, validando os erros 400 e 500

---
+++ Backend/src/server.ts
@@ -0,0 +1,30 @@
+import express ,{Request, Response, NextFunction} from 'express'
+import 'express-async-errors'
+import cors from 'cors';
+
+import { router } from './router';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+//para qualquer ip poder fazer requisição
+
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) =>{
+    if(err instanceof Error){
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+    //Quando  um requisição incorreta for feita no servidor
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+    })
+    // Erro interno no servidor
+
+})
+
+app.listen(3333, () => console.log('Funcionou'));
